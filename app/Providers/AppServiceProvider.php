2b6cb3a1f6725ebd2aab6d989e09bc77e365ad5f
boshraa <boshraayounes@gmail.com>
2021-10-15 08:35:15 +0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+   if($this->app->environment('production')) {
+\URL::forceScheme('https');
 }
